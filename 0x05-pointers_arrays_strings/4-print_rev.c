803b51f6ea6f2e6b456b1f3245fa3a8915013b82
Bosschi22 <bosschi22@gmail.com>
2023-08-21 07:11:58 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main"
+/**
+ * print_rev - print in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int run = 0;
+	int m;
+	while (*s != '\0')
+
+	{
+		run++;
+		s++;
+	}
+	s--
+	for (m = run; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n')
+}
