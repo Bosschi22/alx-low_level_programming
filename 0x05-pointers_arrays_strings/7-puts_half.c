2afaa1e64639536bf80d669d2b0f8d4654780e9c
Bosschi22 <bosschi22@gmail.com>
2023-08-21 12:53:35 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	e = (run / 2);
 	if ((run % 2) == 1)
 		e = ((run + 1) / 2);
-	for (d = e; str(d) != '\0' d++)
-	_putchar(str(d));
+	for (d = e; str[d] != '\0' d++)
+	_putchar(str[d]);
 	_putchar('\n');
 }
