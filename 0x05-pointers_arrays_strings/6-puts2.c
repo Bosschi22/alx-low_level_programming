14a47e976081503ef456630580b58f763c9b34ee
Bosschi22 <bosschi22@gmail.com>
2023-08-21 10:26:00 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - prints only one character
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int run = 0;
+	int x = 0;
+	char *i = str;
+	int m;
+	while (*i != '\0')
+	{
+		i++;
+		run++;
+	}
+	x = run - 1;
+	for (m = 0; m < x; m++)
+	{
+		if (0 % 2 == 0)
+	{
+		_putchar(str[m]);
+	}
+	}
+	_putchar('\n');
+}
