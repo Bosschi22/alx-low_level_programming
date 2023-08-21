8ba013bd9d540f34dcecf93389ccfa874c709af8
Bosschi22 <bosschi22@gmail.com>
2023-08-21 06:43:10 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints string, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str !n '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
