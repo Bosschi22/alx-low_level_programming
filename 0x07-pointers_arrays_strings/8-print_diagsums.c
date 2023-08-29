1f638bad0d4b0abcc143f9e9425f8d5331bd4758
Bosschi22 <bosschi22@gmail.com>
2023-08-29 07:56:21 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,16 @@
 
 void print_diagsums(int *a, int size)
 {
-int sumn, sumx, z;
-	sumn = 0;
-	sumx = 0;
+int sum0, sum1, z;
+	sum0 = 0;
+	sum1 = 0;
 	for (z = 0; z < size; z++)
 	{
-	sumn = sumn + a[z * size + z];
+	sum0 = sum0 + a[z * size + z];
 	}
 	for (z = size - 1; z >= 0; z--)
 	{
 	sumx += a[z * size + (size - y - 1)];
 	}
-	printf("%d, %d\n", sumn, sumx);
+	printf("%d, %d\n", sum0, sum1);
 }
