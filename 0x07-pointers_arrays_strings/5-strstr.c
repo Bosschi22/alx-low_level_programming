e8e77bfb26d0daea367d5c972b6b87e52af8a4eb
Bosschi22 <bosschi22@gmail.com>
2023-08-29 06:05:49 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * char *_strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *c = haystack;
+		char *d = needle;
+
+		while (*c == *d && *d != '\0')
+		{
+			c++;
+			d++;
+		}
+		if (*d == '\0')
+			return (haystack);
+	}
+	return (0);
+}
