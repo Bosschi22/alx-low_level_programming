7477c8e46e3eac5cbc6a6a61f7a0866ef6956046
Bosschi22 <bosschi22@gmail.com>
2023-08-29 01:26:05 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * char *_memcpy - Entry point
+ * @dest: stored memory
+ * @src: copied memory
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int s = 0;
+int x = 0;
+	for (s = 0; s < x; s++)
+	{
+		dest[s] = src[s];
+		n--;
+	}
+	return (dest);
+}
