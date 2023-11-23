ce3ab19c8447c3938c5f3ff191d9f14b6922c247
bosschi <bosschi22@gmail.com>
2023-11-23 12:52:01 -0800

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,15 +11,15 @@
 int *array_range(int min, int max)
 
 {
-int *pr;
+int *ptr;
 int t, size;
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
 	pr = malloc(sizeof(int) * size);
-	if (pr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (t = 0; min <= max; t++)
-		pr[t] = min++;
-	return (pr);
+		ptr[t] = min++;
+	return (ptr);
 }
