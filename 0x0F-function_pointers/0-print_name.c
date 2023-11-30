86e687bb41c5060385c0e6847be925184cac546c
bosschi <bosschi22@gmail.com>
2023-11-30 03:33:32 -0800

    3-main.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,22 @@
 #include "function_pointers.h"
 /**
 *  array_iterator - entry point
-* @array: array to execute function on
+* @array: array to execute func on
 * @size: is the size of the array
-* @action:  is a pointer to the function you need to use
+* @action:  is a pointer to the func you need to use
 *
 * Return: void
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t z;
+size_t i;
 	if (array && action)
 	{
-		z = 0;
-		while (z < size)
+		i = 0;
+		while (i < size)
 		{
-			action(array[z]);
-			z++;
+			action(array[i]);
+			i++;
 		}
 	}
 }
