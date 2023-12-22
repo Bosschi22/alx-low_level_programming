57499bd7ac3de7bf13bcad9d4e65dfe69a6bdda3
Bosschi22 <bosschi22@gmail.com>
2023-12-22 15:41:17 -0800

    create buffer

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 char *create_buffer(char *file);
 void close_file(int fd);
-
 /**
  * creates_buffer - Allocates 1024 bytes for  buf.
  * @file: The name of the file buffer is storing chars for.
@@ -12,14 +11,14 @@ void close_file(int fd);
  */
 char *creates_buffer(char *file)
 {
-char *buf;
-	buf = malloc(sizeof(char) * 1024);
-	if (buf == NULL)
+char *buffer;
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
-	return (buf);
+	return (buffer);
 }
 
 /**
@@ -52,34 +51,35 @@ int ci;
 int main(int argc, char *argv[])
 {
 int from, to, r, w;
-char *buf;
+char *buffer;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buf = create_buffer(argv[2]);
+	buffer = creates_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buf, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
 	do {
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buf);
+			free(buffer);
 			exit(98);
 		}
-		w = write(to, buf, r);
+		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buf);
+			free(buffer);
 			exit(99);
 		}
-		r = read(from, buf, 1024);
+		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-	free(buf);
+	}
+	while (r > 0);
+	free(buffer);
 	close_file(from);
 	close_file(to);
 	return (0);
