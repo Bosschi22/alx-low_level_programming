8129aae662034832ceb7e7c73103157997fb4bd9
Bosschi22 <bosschi22@gmail.com>
2023-12-22 11:31:48 -0800

    100-elf_header.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read txt fil print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buf;
+ssize_t fd;
+ssize_t we;
+ssize_t it;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	it = read(fd, buf, letters);
+	we = write(STDOUT_FILENO, buf, it);
+	free(buf);
+	close(fd);
+	return (we);
+}
